a8da7f127ec256e9febe1d84ff90a607c3d2372c
Matthias Wilhelm <mwilhelm.kl@gmail.com>
2013-11-26 19:32:21 +0100

    Initial commit, let's go with jQM again ...

---
+++ src/me.js
@@ -0,0 +1,14 @@
+/*
+yearSelector = function (li) {
+	var out = li.attr("year");
+	return out;
+}
+
+
+$(document).on("pageinit", "#main", function(){
+    $("#pub-lv").listview({
+        autodividers: true,
+        autodividersSelector: yearSelector
+    }).listview("refresh");
+});
+*/
